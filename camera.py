adc261c148318e18775763b47894e27ed6fa9480
paauulm <landamoyano.2117371@studenti.uniroma1.it>
2024-04-22 13:04:56 +0200

    Mediapipe

--- camera.py
+++ camera.py
@@ -1,4 +1,5 @@
 import cv2
+import mediapipe as mp
 
 # Informar al usuario y obtener consentimiento
 print("Welcome to the air drawing app. This app uses the camera to detect motion and allow you to draw on the screen.")
@@ -8,6 +9,10 @@ if consentimiento.lower() != 'y':
     print("Permission denied. The app cannot access the camera.")
     exit()
 
+mp_hand = mp.solutions.hands
+hands = mp_hand.Hands()
+mp_drawing_utils = mp.solutions.drawing_utils
+
 # Acceder a la cámara y mostrar el video en tiempo real
 cap = cv2.VideoCapture(0)  # Acceder a la primera cámara disponible
 
@@ -16,6 +21,13 @@ while True:
     if not ret:
         break # Exit loop if video capture failed
 
+    result = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+    print(result.multi_hand_landmarks)
+
+    if result.multi_hand_landmarks:
+        for hand_landmark in result.multi_hand_landmarks:
+                mp_drawing_utils.draw_landmarks(frame, hand_landmark, mp_hand.HAND_CONNECTIONS)
     # Mostrar el fotograma capturado en una ventana
     cv2.imshow('Camera', frame)
 
